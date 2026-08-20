9cb4fc489493c49b17b18c5df3689d074c878d80
NatzuF <GabrielCA_19@ciencias.unam.mx>
2026-08-20 11:17:16 -0600

    Crear carpeta para entregar praticas y la practica 01

---
+++ README.md
@@ -0,0 +1,14 @@
+# Universidad Nacional Autónoma de México
+### FACULTAD DE CIENCIAS, 2026-II
+### LENGUAJES DE PROGRAMACIÓN
+
+---
+
+## *Practica01*
+
+---
+
+**Gabriel Candiani Pérez**  
+318268534
+
+*19 de Agosto de 2026*
---
+++ practica01/Laboratorio01.hs
@@ -0,0 +1,39 @@
+module Laboratorio01 where
+
+distanciaOrigen :: Double -> Double -> Double
+distanciaOrigen x y = sqrt (x^2 + y^2)
+
+sumaCuadradosPares :: [Int] -> Int
+sumaCuadradosPares xs = sum (map (^2) (filter even xs))
+
+aplicaTresVeces :: (a -> a) -> a -> a
+aplicaTresVeces f x = f (f (f x))
+
+varianza2 :: Double -> Double -> Double
+varianza2 x y = ((x - media)^2 + (y - media)^2) / 2
+    where
+        media = (x + y) / 2
+
+clasificaTemperatura :: Int -> String
+clasificaTemperatura t
+    | t <= 0    = "frio extremo"
+    | t <= 15   = "frio"
+    | t <= 25   = "templado"
+    | t <= 35   = "calido"
+    | otherwise = "calor extremo"
+
+intercala :: a -> [a] -> [a]
+intercala _ [] = []
+intercala _ [x] = [x]
+intercala sep (x:xs) = x : sep : intercala sep xs
+
+data Expr
+  = Lit Int
+  | Suma Expr Expr
+  | Producto Expr Expr
+  deriving (Eq, Show)
+
+evalua :: Expr -> Int
+evalua (Lit n)          = n
+evalua (Suma e1 e2)     = evalua e1 + evalua e2
+evalua (Producto e1 e2) = evalua e1 * evalua e2
